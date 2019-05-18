1d04bb5b2960681d9473ac2574afe005b752429f
alumezi <arbnorlumezi@gmail.com>
2019-05-18 20:14:11 +0200

    Structure

--- index.js
+++ index.js
@@ -1,31 +1,7 @@
 const express = require("express");
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const keys = require("./config/keys");
-
 const app = express();
-
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.google_client_ID,
-      clientSecret: keys.google_client_secret,
-      callbackURL: "/auth/google/callback"
-    },
-    () => {
-      accessToken => {
-        console.log(accessToken);
-      };
-    }
-  )
-);
-
-app.get(
-  "/auth/google",
-  passport.authenticate("google", {
-    scope: ["profile", "email"]
-  })
-);
+require("./routes/authRoutes")(app);
+require("./services/passport");
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
