7fa2758af2f01fa8fba99ac1e59edfe16bc9c4d0
alumezi <arbnorlumezi@gmail.com>
2019-05-23 19:47:18 +0200

    Last changes

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
+require("./models/User");
+
+mongoose.connect(keys.mongoURI);
+
 require("./routes/authRoutes")(app);
 require("./services/passport");
 
