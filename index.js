7a6504433aa354cbd571c72a3eb8f14d53962a8b
alumezi <arbnorlumezi@gmail.com>
2019-07-04 20:44:57 +0200

    Commit to clean

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const app = express();
 const mongoose = require("mongoose");
 const keys = require("./config/keys");
 require("./models/User");
+require("./services/passport");
 
-mongoose.connect(keys.mongoURI);
+mongoose.connect(keys.mongoURI)
 
 require("./routes/authRoutes")(app);
-require("./services/passport");
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
