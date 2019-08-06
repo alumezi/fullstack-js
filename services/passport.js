0e1bbdcfe27e44e810d857d712f9e46ce3e414e4
alumezi <arbnorlumezi@gmail.com>
2019-08-06 14:47:21 +0200

    039-040

--- services/passport.js
+++ services/passport.js
@@ -13,7 +13,15 @@ passport.use(
       callbackURL: "/auth/google/callback"
     },
     (accessToken, refreshToken, profile, done) => {
-      new User({ googleId : profile.id, displayName : profile.displayName, emails : profile.emails }).save()
+      User.findOne({ googleId : profile.id }).then(existingUser => {
+        if(existingUser){
+          done(null, existingUser)
+        }else{
+          new User({ googleId : profile.id, displayName : profile.displayName, emails : profile.emails })
+          .save()
+          .then(user => done(null, user))
+        }
+      })
     }
   )
 )
