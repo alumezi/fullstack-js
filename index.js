16629b0a09d69fdbe5b478cb7314693f8f16b2d2
alumezi <arbnorlumezi@gmail.com>
2019-09-07 20:26:38 +0200

    add app.use for cookies

--- index.js
+++ index.js
@@ -6,14 +6,17 @@ const keys = require("./config/keys");
 require("./models/User");
 require("./services/passport");
 
-mongoose.connect(keys.mongoURI)
+mongoose.connect(keys.mongoURI);
 
 app.use(
     cookieSession({
         maxAge: 30 * 24 * 60 * 60 * 1000,
         keys: [keys.cookieKey]
     })
-)
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 require("./routes/authRoutes")(app);
 
