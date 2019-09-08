d2b2b70687c9c82df33846440a72523ae59be17c
alumezi <arbnorlumezi@gmail.com>
2019-09-08 18:17:58 +0200

    proxy:true

--- services/passport.js
+++ services/passport.js
@@ -22,7 +22,8 @@ passport.use(
     {
       clientID: keys.google_client_ID,
       clientSecret: keys.google_client_secret,
-      callbackURL: "/auth/google/callback"
+      callbackURL: "/auth/google/callback",
+      proxy : true
     },
     (accessToken, refreshToken, profile, done) => {
       User.findOne({ googleId: profile.id }).then(existingUser => {
