66af7c99742f0dc8ca74828f8cb857c8129088b3
alumezi <arbnorlumezi@gmail.com>
2019-09-08 14:06:44 +0200

    DEV & PROD separation

---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+    google_client_ID: process.env.GOOGLE_CLIENT_ID,
+    google_client_secret: process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI: process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY
+};
