c11f364e7f24e4d0251f6497c4c16553841b0dbc
alumezi <arbnorlumezi@gmail.com>
2019-09-10 22:49:47 +0200

    first component

--- Untitled-1.js
+++
@@ -1,7 +0,0 @@
-const fetchAllAlbums = async () => {
-    const response = await fetch('https://rallycoding.herokuapp/api/music_albums')
-    const json = await response.json()
-
-    console.log("TCL: fetchAllAlbums -> json", json)
-}
-fetchAllAlbums();
